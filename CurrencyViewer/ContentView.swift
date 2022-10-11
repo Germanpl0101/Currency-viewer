018041aaef480e10296578f65fce8b8d65c9dc36
German Blonski <german@inoffices.net>
2022-10-11 14:13:34 +0200

    Initial Commit

---
+++ CurrencyViewer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CurrencyViewer
+//
+//  Created by German Blonski on 11.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CurrencyViewer/CurrencyViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyViewerApp.swift
+//  CurrencyViewer
+//
+//  Created by German Blonski on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
